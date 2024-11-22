669839598cfd17d4aa3fef57f823f4219b0213ab
Will MacCormack <willmaccormack@gmail.com>
2024-11-22 16:09:09 -0500

    Tweaks to make the Demo Work better w/o an io el

--- main.v
+++ main.v
@@ -5,18 +5,30 @@ module top(
     output [23:0] IO_LED
 );
 
-    localparam COUNTER_WIDTH = 32 + 8;
+    localparam NUM_LEDS = 32;	
+    localparam TIMING_OFFSET = 24;
+    localparam COUNTER_WIDTH = NUM_LEDS + TIMING_OFFSET;
     reg [COUNTER_WIDTH-1:0] counter;
+
+    reg [7:0] shift;
+
 
     always @(posedge clk) begin
         if (rst == 1'b0) begin
             counter <= 0;
+	    shift <= '0;
         end else begin
             counter <= counter + 1;
+	    if (counter[TIMING_OFFSET-1:0] == TIMING_OFFSET'd0) begin
+		shift[7:0] <= {shift[6:0], ~| shift[6:0]};
+	    end
         end
     end
 
-    assign LED = counter[COUNTER_WIDTH-1:COUNTER_WIDTH-8];
-    assign IO_LED = counter[COUNTER_WIDTH-9:COUNTER_WIDTH-(8 + 24)];
+    assign IO_LED = counter[TIMING_OFFSET+3+7:TIMING_OFFSET+3];
+    assign LED = shift;
+    // assign LED = counter[COUNTER_WIDTH-(24 + 1):COUNTER_WIDTH-32];
+
+
 
 endmodule
